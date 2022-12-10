69eebde6277ac481c6624fd6ad8f08c61162161a
wellingtonvidaleal <wvidaleal@gmail.com>
2022-12-10 19:07:36 -0300

    Configuração do Prometheus funcionando com a criação e passagem por parâmetro do usuário iam do prometheus.

--- prometheus.tf
+++ prometheus.tf
@@ -49,6 +49,16 @@ resource "aws_security_group" "prometheus" {
   }
 }
 
+locals {
+  user_data_prometheus = templatefile(
+    "${path.module}/templates/user_data_prometheus.tftpl",
+    {
+      prometheus_iam_access_key = aws_iam_access_key.prometheus.id
+      prometheus_iam_secret_key = aws_iam_access_key.prometheus.secret
+    }
+  )
+}
+
 #Define o EC2 do Prometheus
 resource "aws_instance" "prometheus" {
   ami                         = var.ami_wordpress
@@ -59,49 +69,83 @@ resource "aws_instance" "prometheus" {
   subnet_id                   = aws_subnet.public_az_a.id
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update && sudo apt install git curl ansible unzip -y
-    cd /tmp
-    git clone https://github.com/wellingtonvidaleal/ansible-desafio-final-formacao-sre.git
-    cd ansible-desafio-final-formacao-sre
-
-    sudo ansible-playbook prometheus.yml \
-      --inventory 'localhost,' \
-      --connection local
-  EOF
-
-  # user_data = <<-EOF
-  #   #!/bin/bash
-  #   sudo apt-get update
-  #   sudo apt-get install \
-  #   ca-certificates \
-  #   curl \
-  #   gnupg \
-  #   lsb-release
-  #   sudo mkdir -p /etc/apt/keyrings
-  #   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  #   echo \
-  #   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  #   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  #   sudo apt-get update
-  #   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-  #   docker run -p 9090:9090 prom/prometheus
-
-  # EOF  
-
-  # user_data = <<-EOF
-  #   #!/bin/bash
-  #   sudo apt update && sudo apt install curl -y
-  #   cd ~/
-  #   wget https://github.com/prometheus/prometheus/releases/download/v2.40.5/prometheus-2.40.5.linux-amd64.tar.gz
-  #   tar xvfz prometheus-*.tar.gz
-  #   cd prometheus-2.40.5.linux-amd64
-  #   ./prometheus --config.file=prometheus.yml
-  # EOF
+  user_data = base64encode(local.user_data_prometheus)
 
   tags = {
     Name = "prometheus"
   }
+}
+
+resource "aws_iam_group" "monitoring" {
+  name = "monitoring"
+  path = "/users/"
+}
+
+resource "aws_iam_user" "prometheus" {
+  name = "prometheus"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_access_key" "prometheus" {
+  user = aws_iam_user.prometheus.name
+}
+
+resource "aws_iam_user_policy" "prometheus_ro" {
+  name = "prometheus_ro"
+  user = aws_iam_user.prometheus.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "ec2:Describe*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "elasticloadbalancing:Describe*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:ListMetrics",
+                "cloudwatch:GetMetricStatistics",
+                "cloudwatch:Describe*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "autoscaling:Describe*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_group_membership" "monitorin_prometheus" {
+  name = "monitorin_prometheus"
+
+  users = [
+    aws_iam_user.prometheus.name,
+  ]
+
+  group = aws_iam_group.monitoring.name
+}
+
+output "prometheus_access_key" {
+  value = aws_iam_access_key.prometheus.id
+}
+
+output "prometheus_secret_key" {
+  value = aws_iam_access_key.prometheus.secret
+  sensitive = true
 }
